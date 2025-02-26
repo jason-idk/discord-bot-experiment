b0e9a70fbd24970a6ae595c2121ecb0d230411fe
josh-hensley <me@joshhensley.com>
2025-02-26 16:28:25 -0600

    set up some simple commands

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
- const { Client, Events, GatewayIntentBits } = require('discord.js');
- const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
- const token = 'YOUR_BOT_TOKEN'; 
- 
- client.on(Events.ClientReady, () => {
-     console.log(`Logged in as ${client.user.tag}!`);
- });
- 
- client.on(Events.MessageCreate, message => {
-     if (message.content === 'ping') {
-         message.reply('pong!');
-     }
- });
- 
- client.login(token);
+import { REST, Routes, Client, Events, GatewayIntentBits } from 'discord.js';
+import dotenv from 'dotenv';
+
+dotenv.config()
+const { TOKEN, CLIENT_ID } = process.env
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+  {
+    name: 'suggest_movie',
+    description: 'finds movies in theatres and makes a suggestion',
+  }
+];
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+try {
+  console.log('Started refreshing application (/) commands.');
+
+  await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+
+  console.log('Successfully reloaded application (/) commands.');
+} catch (error) {
+  console.error(error);
+}
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on(Events.ClientReady, readyClient => {
+  console.log(`Logged in as ${readyClient.user.tag}!`);
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+  if (interaction.commandName === 'suggest_movie') {
+    await interaction.reply('Jaws!');
+  }
+});
+
+client.login(TOKEN); 
